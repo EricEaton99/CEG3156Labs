47b22d4ef8e3ac007e5699a7cf49182249907fd2
Eric Eaton <eric@propelvr.ca>
2024-03-09 02:42:22 -0500

    working ALU

--- Lab2/simulation/qsim/work/top_level/_primary.vhd
+++ Lab2/simulation/qsim/work/top_level/_primary.vhd
@@ -2,14 +2,12 @@ library verilog;
 use verilog.vl_types.all;
 entity top_level is
     port(
-        reg1_data       : out    vl_logic_vector(31 downto 0);
-        clk             : in     vl_logic;
-        global_reset    : in     vl_logic;
-        write_en        : in     vl_logic;
-        reg1_adr        : in     vl_logic_vector(4 downto 0);
-        reg2_adr        : in     vl_logic_vector(4 downto 0);
-        write_adr       : in     vl_logic_vector(4 downto 0);
-        write_data      : in     vl_logic_vector(31 downto 0);
-        reg2_data       : out    vl_logic_vector(31 downto 0)
+        zero            : out    vl_logic;
+        A               : in     vl_logic_vector(31 downto 0);
+        B               : in     vl_logic_vector(31 downto 0);
+        op_sel          : in     vl_logic_vector(2 downto 0);
+        c_out           : out    vl_logic;
+        overflow        : out    vl_logic;
+        result          : out    vl_logic_vector(31 downto 0)
     );
 end top_level;
